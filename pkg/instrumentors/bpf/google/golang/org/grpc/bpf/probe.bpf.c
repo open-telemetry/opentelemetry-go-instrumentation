6f3838c102c331d110530efbbfdead2872430e9e
Eden Federman <eden@keyval.dev>
2022-10-31 11:10:33 +0200

    add attribution

--- pkg/instrumentors/bpf/google/golang/org/grpc/bpf/probe.bpf.c
+++ pkg/instrumentors/bpf/google/golang/org/grpc/bpf/probe.bpf.c
@@ -1,3 +1,17 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #include "arguments.h"
 #include "go_types.h"
 #include "span_context.h"
@@ -9,7 +23,8 @@ char __license[] SEC("license") = "Dual MIT/GPL";
 #define MAX_CONCURRENT 50
 #define MAX_HEADERS_BUFF_SIZE 500
 
-struct grpc_request_t {
+struct grpc_request_t
+{
     u64 start_time;
     u64 end_time;
     char method[MAX_SIZE];
@@ -18,32 +33,37 @@ struct grpc_request_t {
     struct span_context psc;
 };
 
-struct hpack_header_field {
+struct hpack_header_field
+{
     struct go_string name;
     struct go_string value;
     bool sensitive;
 };
 
-struct {
-	__uint(type, BPF_MAP_TYPE_HASH);
-	__type(key, void*);
-	__type(value, struct grpc_request_t);
-	__uint(max_entries, MAX_CONCURRENT);
+struct
+{
+    __uint(type, BPF_MAP_TYPE_HASH);
+    __type(key, void *);
+    __type(value, struct grpc_request_t);
+    __uint(max_entries, MAX_CONCURRENT);
 } context_to_grpc_events SEC(".maps");
 
-struct headers_buff {
+struct headers_buff
+{
     unsigned char buff[MAX_HEADERS_BUFF_SIZE];
 };
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
-	__type(key, s32);
-	__type(value, struct headers_buff);
-	__uint(max_entries, 1);
+struct
+{
+    __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+    __type(key, s32);
+    __type(value, struct headers_buff);
+    __uint(max_entries, 1);
 } headers_buff_map SEC(".maps");
 
-struct {
-	__uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+struct
+{
+    __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
 } events SEC(".maps");
 
 // Injected in init
@@ -52,7 +72,8 @@ volatile const u64 clientconn_target_ptr_pos;
 // This instrumentation attaches uprobe to the following function:
 // func (cc *ClientConn) Invoke(ctx context.Context, method string, args, reply interface{}, opts ...CallOption) error
 SEC("uprobe/ClientConn_Invoke")
-int uprobe_ClientConn_Invoke(struct pt_regs *ctx) {
+int uprobe_ClientConn_Invoke(struct pt_regs *ctx)
+{
     // positions
     u64 clientconn_pos = 1;
     u64 context_pos = 3;
@@ -63,18 +84,18 @@ int uprobe_ClientConn_Invoke(struct pt_regs *ctx) {
     grpcReq.start_time = bpf_ktime_get_boot_ns();
 
     // Read Method
-    void* method_ptr = get_argument(ctx, method_ptr_pos);
-    u64 method_len = (u64) get_argument(ctx, method_len_pos);
+    void *method_ptr = get_argument(ctx, method_ptr_pos);
+    u64 method_len = (u64)get_argument(ctx, method_len_pos);
     u64 method_size = sizeof(grpcReq.method);
     method_size = method_size < method_len ? method_size : method_len;
     bpf_probe_read(&grpcReq.method, method_size, method_ptr);
 
     // Read ClientConn.Target
-    void* clientconn_ptr = get_argument(ctx, clientconn_pos);
-    void* target_ptr = 0;
-    bpf_probe_read(&target_ptr, sizeof(target_ptr), (void *)(clientconn_ptr+(clientconn_target_ptr_pos)));
+    void *clientconn_ptr = get_argument(ctx, clientconn_pos);
+    void *target_ptr = 0;
+    bpf_probe_read(&target_ptr, sizeof(target_ptr), (void *)(clientconn_ptr + (clientconn_target_ptr_pos)));
     u64 target_len = 0;
-    bpf_probe_read(&target_len, sizeof(target_len), (void *)(clientconn_ptr+(clientconn_target_ptr_pos+8)));
+    bpf_probe_read(&target_len, sizeof(target_len), (void *)(clientconn_ptr + (clientconn_target_ptr_pos + 8)));
     u64 target_size = sizeof(grpcReq.target);
     target_size = target_size < target_len ? target_size : target_len;
     bpf_probe_read(&grpcReq.target, target_size, target_ptr);
@@ -86,10 +107,11 @@ int uprobe_ClientConn_Invoke(struct pt_regs *ctx) {
 }
 
 SEC("uprobe/ClientConn_Invoke")
-int uprobe_ClientConn_Invoke_Returns(struct pt_regs *ctx) {
+int uprobe_ClientConn_Invoke_Returns(struct pt_regs *ctx)
+{
     u64 context_pos = 3;
     void *context_ptr = get_argument(ctx, context_pos);
-    void* grpcReq_ptr = bpf_map_lookup_elem(&context_to_grpc_events, &context_ptr);
+    void *grpcReq_ptr = bpf_map_lookup_elem(&context_to_grpc_events, &context_ptr);
     struct grpc_request_t grpcReq = {};
     bpf_probe_read(&grpcReq, sizeof(grpcReq), grpcReq_ptr);
 
@@ -102,48 +124,55 @@ int uprobe_ClientConn_Invoke_Returns(struct pt_regs *ctx) {
 
 // func (t *http2Client) createHeaderFields(ctx context.Context, callHdr *CallHdr) ([]hpack.HeaderField, error)
 SEC("uprobe/Http2Client_createHeaderFields")
-int uprobe_Http2Client_CreateHeaderFields(struct pt_regs *ctx) {
+int uprobe_Http2Client_CreateHeaderFields(struct pt_regs *ctx)
+{
     // Read slice
     s32 context_pointer_pos = 3;
     struct go_slice slice = {};
     struct go_slice_user_ptr slice_user_ptr = {};
-    if (is_registers_abi) {
-        slice.array = (void*) ctx->rax;
-        slice.len = (s32) ctx->rbx;
-        slice.cap = (s32) ctx->rcx;
+    if (is_registers_abi)
+    {
+        slice.array = (void *)ctx->rax;
+        slice.len = (s32)ctx->rbx;
+        slice.cap = (s32)ctx->rcx;
         slice_user_ptr.array = &ctx->rax;
         slice_user_ptr.len = &ctx->rbx;
         slice_user_ptr.cap = &ctx->rcx;
-    } else {
+    }
+    else
+    {
         u64 slice_pointer_pos = 5;
         s32 slice_len_pos = 6;
         s32 slice_cap_pos = 7;
         slice.array = get_argument(ctx, slice_pointer_pos);
-        slice.len = (s32) get_argument(ctx, slice_len_pos);
-        slice.cap = (s32) get_argument(ctx, slice_cap_pos);
-        slice_user_ptr.array = (void*)ctx->rsp+(slice_pointer_pos*8);
-        slice_user_ptr.len = (void*)ctx->rsp+(slice_len_pos*8);
-        slice_user_ptr.cap = (void*)ctx->rsp+(slice_cap_pos*8);
+        slice.len = (s32)get_argument(ctx, slice_len_pos);
+        slice.cap = (s32)get_argument(ctx, slice_cap_pos);
+        slice_user_ptr.array = (void *)ctx->rsp + (slice_pointer_pos * 8);
+        slice_user_ptr.len = (void *)ctx->rsp + (slice_len_pos * 8);
+        slice_user_ptr.cap = (void *)ctx->rsp + (slice_cap_pos * 8);
     }
     char key[11] = "traceparent";
     struct go_string key_str = write_user_go_string(key, sizeof(key));
 
     // Get grpc request struct
     void *context_ptr = 0;
-    bpf_probe_read(&context_ptr, sizeof(context_ptr), (void *)(ctx->rsp+(context_pointer_pos*8)));
+    bpf_probe_read(&context_ptr, sizeof(context_ptr), (void *)(ctx->rsp + (context_pointer_pos * 8)));
     void *parent_ctx = find_context_in_map(context_ptr, &context_to_grpc_events);
-    void* grpcReq_ptr = bpf_map_lookup_elem(&context_to_grpc_events, &parent_ctx);
+    void *grpcReq_ptr = bpf_map_lookup_elem(&context_to_grpc_events, &parent_ctx);
     struct grpc_request_t grpcReq = {};
     bpf_probe_read(&grpcReq, sizeof(grpcReq), grpcReq_ptr);
 
     // Get parent if exists
     void *parent_span_ctx = find_context_in_map(context_ptr, &spans_in_progress);
-    if (parent_span_ctx != NULL) {
-        void* psc_ptr = bpf_map_lookup_elem(&spans_in_progress, &parent_span_ctx);
+    if (parent_span_ctx != NULL)
+    {
+        void *psc_ptr = bpf_map_lookup_elem(&spans_in_progress, &parent_span_ctx);
         bpf_probe_read(&grpcReq.psc, sizeof(grpcReq.psc), psc_ptr);
         copy_byte_arrays(grpcReq.psc.TraceID, grpcReq.sc.TraceID, TRACE_ID_SIZE);
         generate_random_bytes(grpcReq.sc.SpanID, SPAN_ID_SIZE);
-    } else {
+    }
+    else
+    {
         grpcReq.sc = generate_span_context();
     }
 
--- pkg/opentelemetry/controller.go
+++ pkg/opentelemetry/controller.go
@@ -1,8 +1,26 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package opentelemetry
 
 import (
 	"context"
 	"fmt"
+	"os"
+	"runtime"
+	"time"
+
 	"github.com/keyval-dev/opentelemetry-go-instrumentation/pkg/instrumentors/events"
 	"github.com/keyval-dev/opentelemetry-go-instrumentation/pkg/log"
 	"github.com/prometheus/procfs"
@@ -13,9 +31,6 @@ import (
 	"go.opentelemetry.io/otel/trace"
 	"golang.org/x/sys/unix"
 	"google.golang.org/grpc"
-	"os"
-	"runtime"
-	"time"
 )
 
 const (
