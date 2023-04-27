55b2a44fd7542fb4c8ce80c2fc01235abbe473c1
Tyler Yahn <MrAlias@users.noreply.github.com>
2023-04-27 14:25:01 -0700

    [chore] Lint fixes for pkg/process (#111)
    * Lint fixes for pkg/process
    
    * Apply suggestions from code review
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    
    ---------
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- pkg/process/discover.go
+++ pkg/process/discover.go
@@ -27,19 +27,23 @@ import (
 	"go.opentelemetry.io/auto/pkg/log"
 )
 
-type processAnalyzer struct {
+// Analyzer is used to find actively running processes.
+type Analyzer struct {
 	done          chan bool
 	pidTickerChan <-chan time.Time
 }
 
-func NewAnalyzer() *processAnalyzer {
-	return &processAnalyzer{
+// NewAnalyzer returns a new [ProcessAnalyzer].
+func NewAnalyzer() *Analyzer {
+	return &Analyzer{
 		done:          make(chan bool, 1),
 		pidTickerChan: time.NewTicker(2 * time.Second).C,
 	}
 }
 
-func (a *processAnalyzer) DiscoverProcessID(target *TargetArgs) (int, error) {
+// DiscoverProcessID searches for the target as an actively running process,
+// returning its PID if found.
+func (a *Analyzer) DiscoverProcessID(target *TargetArgs) (int, error) {
 	for {
 		select {
 		case <-a.done:
@@ -47,21 +51,20 @@ func (a *processAnalyzer) DiscoverProcessID(target *TargetArgs) (int, error) {
 			return 0, errors.ErrInterrupted
 		case <-a.pidTickerChan:
 			pid, err := a.findProcessID(target)
-			if err != nil {
-				if err == errors.ErrProcessNotFound {
-					log.Logger.V(0).Info("process not found yet, trying again soon", "exe_path", target.ExePath)
-				} else {
-					log.Logger.Error(err, "error while searching for process", "exe_path", target.ExePath)
-				}
-			} else {
+			if err == nil {
 				log.Logger.V(0).Info("found process", "pid", pid)
 				return pid, nil
+			}
+			if err == errors.ErrProcessNotFound {
+				log.Logger.V(0).Info("process not found yet, trying again soon", "exe_path", target.ExePath)
+			} else {
+				log.Logger.Error(err, "error while searching for process", "exe_path", target.ExePath)
 			}
 		}
 	}
 }
 
-func (a *processAnalyzer) findProcessID(target *TargetArgs) (int, error) {
+func (a *Analyzer) findProcessID(target *TargetArgs) (int, error) {
 	proc, err := os.Open("/proc")
 	if err != nil {
 		return 0, err
@@ -111,6 +114,7 @@ func (a *processAnalyzer) findProcessID(target *TargetArgs) (int, error) {
 	return 0, errors.ErrProcessNotFound
 }
 
-func (a *processAnalyzer) Close() {
+// Close closes the analyzer.
+func (a *Analyzer) Close() {
 	a.done <- true
 }
