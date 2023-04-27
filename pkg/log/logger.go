00920ee558415c0a6e556e7eb0c87598ef7986fb
Tyler Yahn <MrAlias@users.noreply.github.com>
2023-04-27 15:58:20 -0700

    [chore] Lint fixes for pkg/log (#113)
    * Lint fixes for pkg/log
    
    * Update pkg/log/logger.go
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>
    
    ---------
    
    Co-authored-by: Robert Pająk <pellared@hotmail.com>

--- pkg/log/logger.go
+++ pkg/log/logger.go
@@ -20,8 +20,10 @@ import (
 	"go.uber.org/zap"
 )
 
+// Logger is a global logger used for internal logging.
 var Logger logr.Logger
 
+// Init initializes [Logger] to be a production Zap logger.
 func Init() error {
 	zapLog, err := zap.NewProduction()
 	if err != nil {
