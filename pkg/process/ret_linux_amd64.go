7663e44f9807b38d9e6f7d999457e3ca1f736888
Mike Goldsmth <goldsmith.mike@gmail.com>
2023-05-02 11:51:08 +0100

    add build guards to findReturnInstructions arch files

--- pkg/process/ret_linux_amd64.go
+++ pkg/process/ret_linux_amd64.go
@@ -11,6 +11,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//go:build 386 || amd64
+// +build 386 amd64
 
 package process
 
