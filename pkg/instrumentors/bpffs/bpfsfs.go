6c125eefbfe5d218a91b197b55f66f68032e8567
Eden Federman <eden@keyval.dev>
2023-07-05 13:49:22 +0300

    BPF FS: add support for multiple processes

--- pkg/instrumentors/bpffs/bpfsfs.go
+++ pkg/instrumentors/bpffs/bpfsfs.go
@@ -14,5 +14,51 @@
 
 package bpffs
 
+import (
+	"fmt"
+	"os"
+
+	"go.opentelemetry.io/auto/pkg/log"
+
+	"golang.org/x/sys/unix"
+
+	"go.opentelemetry.io/auto/pkg/process"
+)
+
 // BPFFsPath is the system path to the BPF file-system.
-const BPFFsPath = "/sys/fs/bpf"
+const bpfFsPath = "/sys/fs/bpf"
+
+func PathForTargetApplication(target *process.TargetDetails) string {
+	return fmt.Sprintf("%s/%d", bpfFsPath, target.PID)
+}
+
+func Mount(target *process.TargetDetails) error {
+	if !isBPFFSMounted() {
+		// Directory does not exist, create it and mount
+		if err := os.MkdirAll(bpfFsPath, 0755); err != nil {
+			return err
+		}
+
+		err := unix.Mount(bpfFsPath, bpfFsPath, "bpf", 0, "")
+		if err != nil {
+			return err
+		}
+	}
+
+	return os.Mkdir(PathForTargetApplication(target), 0755)
+}
+
+func isBPFFSMounted() bool {
+	var stat unix.Statfs_t
+	err := unix.Statfs(bpfFsPath, &stat)
+	if err != nil {
+		log.Logger.Error(err, "failed to statfs bpf filesystem")
+		return false
+	}
+
+	return stat.Type == unix.BPF_FS_MAGIC
+}
+
+func Cleanup(target *process.TargetDetails) error {
+	return os.RemoveAll(PathForTargetApplication(target))
+}
