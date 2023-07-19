0b3a9d3c3bc1736d510159e5e62f727814ed2901
Ron Federman <ronfederman@mail.tau.ac.il>
2023-07-19 16:53:31 +0300

    Adding demo for http server plus db query

---
+++ examples/httpPlusdb/main.go
@@ -0,0 +1,96 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"database/sql"
+	_ "github.com/mattn/go-sqlite3"
+	"go.uber.org/zap"
+)
+
+const sql_query = "SELECT * FROM contacts"
+
+// Server is Http server that exposes multiple endpoints.
+type Server struct {
+	db *sql.DB
+}
+
+// NewServer creates a server struct after initialing rand.
+func NewServer() *Server {
+	database, err := sql.Open("sqlite3", "test.db")
+	if err != nil {
+		panic(err)
+	}
+
+	return &Server{
+		db: database,
+	}
+}
+
+func (s *Server) queryDb(w http.ResponseWriter, req *http.Request) {
+	ctx := req.Context()
+
+	conn, err := s.db.Conn(ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	rows, err := conn.QueryContext(req.Context(), sql_query)
+	if err != nil {
+		panic(err)
+	}
+
+	logger.Info("queryDb called")
+	for rows.Next() {
+		var id int
+		var first_name string
+		var last_name string
+		var email string
+		var phone string
+		err := rows.Scan(&id, &first_name, &last_name, &email, &phone)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Fprintf(w, "ID: %d, first_name: %s, last_name: %s, email: %s, phone: %s\n", id, first_name, last_name, email, phone)
+	}
+}
+
+var logger *zap.Logger
+
+func setupHandler(s *Server) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/query_db", s.queryDb)
+	return mux
+}
+
+func main() {
+	var err error
+	logger, err = zap.NewDevelopment()
+	if err != nil {
+		fmt.Printf("error creating zap logger, error:%v", err)
+		return
+	}
+	port := fmt.Sprintf(":%d", 8080)
+	logger.Info("starting http server", zap.String("port", port))
+
+	s := NewServer()
+	mux := setupHandler(s)
+	if err := http.ListenAndServe(port, mux); err != nil {
+		logger.Error("error running server", zap.Error(err))
+	}
+}
