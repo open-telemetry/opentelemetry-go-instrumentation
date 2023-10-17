e6cdeb25cc0539d8503a6f082db7bd6563b0ccd7
Tyler Yahn <codingalias@gmail.com>
2023-10-17 14:02:17 -0700

    Add offsets package
    Intended to be a standalone version of the inject package data model.

---
+++ internal/pkg/offsets/offsets.go
@@ -0,0 +1,291 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package offsets
+
+import (
+	"encoding/json"
+	"sort"
+	"sync"
+
+	"github.com/hashicorp/go-version"
+)
+
+// Index holds all strict field offsets.
+type Index map[ID]*Offsets
+
+// UnmarshalJSON unmarshals the offset JSON data into i.
+func (i *Index) UnmarshalJSON(data []byte) error {
+	var pkgs []*jsonPackage
+	err := json.Unmarshal(data, &pkgs)
+	if err != nil {
+		return err
+	}
+
+	m := make(map[ID]*Offsets)
+
+	for _, p := range pkgs {
+		for _, s := range p.Structs {
+			for _, f := range s.Fields {
+				for _, o := range f.Offsets {
+					for _, v := range o.Versions {
+						key := ID{
+							PkgPath: p.Package,
+							Struct:  s.Struct,
+							Field:   f.Field,
+						}
+
+						off, ok := m[key]
+						if !ok {
+							off = new(Offsets)
+							m[key] = off
+						}
+						off.Put(v, o.Offset)
+					}
+				}
+			}
+		}
+	}
+
+	*i = m
+
+	return nil
+}
+
+// MarshalJSON marshals i into JSON data.
+func (i Index) MarshalJSON() ([]byte, error) {
+	var out []*jsonPackage
+	for id, off := range i {
+		jp := find(&out, func(p *jsonPackage) bool {
+			return id.PkgPath == p.Package
+		})
+		jp.Package = id.PkgPath
+		jp.addOffsets(id.Struct, id.Field, off)
+	}
+
+	// Ensure repeatability by sorting.
+	for _, p := range out {
+		for _, s := range p.Structs {
+			for _, f := range s.Fields {
+				sort.Slice(f.Offsets, func(i, j int) bool {
+					return f.Offsets[i].Offset < f.Offsets[j].Offset
+				})
+			}
+			sort.Slice(s.Fields, func(i, j int) bool {
+				return s.Fields[i].Field < s.Fields[j].Field
+			})
+		}
+		sort.Slice(p.Structs, func(i, j int) bool {
+			return p.Structs[i].Struct < p.Structs[j].Struct
+		})
+	}
+	sort.Slice(out, func(i, j int) bool {
+		return out[i].Package < out[j].Package
+	})
+
+	return json.Marshal(out)
+}
+
+// ID is a struct field identifier for an offset.
+type ID struct {
+	// PkgPath package import path containing the struct field.
+	PkgPath string
+	// Struct is the name of the struct containing the field.
+	Struct string
+	// Field is the field name.
+	Field string
+}
+
+// Offsets are the byte offsets for a struct field at specific versions of the
+// package containing struct.
+type Offsets struct {
+	// mu ensures synchronous access to all Offsets fields.
+	mu sync.RWMutex
+
+	// values is a map between version and offset value.
+	values map[verKey]offsetVersion
+}
+
+// Get returns the offset in bytes and true if known. Otherwise, 0 and false
+// are returned.
+func (o *Offsets) Get(ver *version.Version) (uint64, bool) {
+	o.mu.RLock()
+	v, ok := o.values[newVerKey(ver)]
+	o.mu.RUnlock()
+	return v.offset, ok
+}
+
+// Put sets the offset value for ver. If an offset for ver is already known
+// (i.e. ver.Equal(other) == true), this will overwrite that value.
+func (o *Offsets) Put(ver *version.Version, offset uint64) {
+	ov := offsetVersion{offset: offset, version: ver}
+
+	o.mu.Lock()
+	defer o.mu.Unlock()
+
+	if o.values == nil {
+		o.values = map[verKey]offsetVersion{newVerKey(ver): ov}
+		return
+	}
+
+	o.values[newVerKey(ver)] = ov
+}
+
+func (o *Offsets) index() map[uint64][]*version.Version {
+	o.mu.RLock()
+	defer o.mu.RUnlock()
+
+	out := make(map[uint64][]*version.Version)
+	for _, ov := range o.values {
+		vers, ok := out[ov.offset]
+		if ok {
+			i := sort.Search(len(vers), func(i int) bool {
+				return vers[i].GreaterThanOrEqual(ov.version)
+			})
+			vers = append(vers, nil)
+			copy(vers[i+1:], vers[i:])
+			vers[i] = ov.version
+		} else {
+			vers = append(vers, ov.version)
+		}
+		out[ov.offset] = vers
+	}
+	return out
+}
+
+type verKey struct {
+	major, minor, patch uint64
+	prerelease          string
+	metadata            string
+}
+
+func newVerKey(v *version.Version) verKey {
+	var segs [3]int
+	copy(segs[:], v.Segments())
+	return verKey{
+		major:      uint64(segs[0]),
+		minor:      uint64(segs[1]),
+		patch:      uint64(segs[2]),
+		prerelease: v.Prerelease(),
+		metadata:   v.Metadata(),
+	}
+}
+
+type offsetVersion struct {
+	offset  uint64
+	version *version.Version
+}
+
+type jsonOffset struct {
+	Offset   uint64             `json:"offset"`
+	Versions []*version.Version `json:"versions"`
+}
+
+type jsonField struct {
+	Field   string        `json:"field"`
+	Offsets []*jsonOffset `json:"offsets"`
+}
+
+func (jf *jsonField) addOffsets(off *Offsets) {
+	for o, vers := range off.index() {
+		jOff := find(&jf.Offsets, func(jo *jsonOffset) bool {
+			return o == jo.Offset
+		})
+		jOff.Offset = o
+
+		jOff.Versions = mergeSorted(jOff.Versions, vers, func(a, b *version.Version) int {
+			return a.Compare(b)
+		})
+	}
+}
+
+type jsonStruct struct {
+	Struct string       `json:"struct"`
+	Fields []*jsonField `json:"fields"`
+}
+
+func (js *jsonStruct) addOffsets(field string, off *Offsets) {
+	jf := find(&js.Fields, func(f *jsonField) bool {
+		return field == f.Field
+	})
+	jf.Field = field
+	jf.addOffsets(off)
+}
+
+type jsonPackage struct {
+	Package string        `json:"package"`
+	Structs []*jsonStruct `json:"structs"`
+}
+
+func (jp *jsonPackage) addOffsets(strct, field string, off *Offsets) {
+	js := find(&jp.Structs, func(s *jsonStruct) bool {
+		return strct == s.Struct
+	})
+	js.Struct = strct
+	js.addOffsets(field, off)
+}
+
+// find returns the value in slice where f evaluates to true. If none exists a
+// new value of *T is created and appended to slice.
+func find[T any](slice *[]*T, f func(*T) bool) *T {
+	var t *T
+	for _, s := range *slice {
+		if f(s) {
+			t = s
+			break
+		}
+	}
+	if t == nil {
+		t = new(T)
+		*slice = append(*slice, t)
+	}
+	return t
+}
+
+// mergeSorted merges the two sorted slices slice0 and slice1 using the cmp
+// function to compare elements.
+//
+// The cmp function needs to return negative values when a<b, possitive values
+// when a>b, and 0 when a==b.
+func mergeSorted[T any](slice0, slice1 []T, cmp func(a, b T) int) []T {
+	merged := make([]T, 0, len(slice0)+len(slice1))
+	i, j := 0, 0
+
+	for i < len(slice0) && j < len(slice1) {
+		switch c := cmp(slice0[i], slice1[j]); {
+		case c < 0:
+			merged = append(merged, slice0[i])
+			i++
+		case c > 0:
+			merged = append(merged, slice1[j])
+			j++
+		case c == 0:
+			merged = append(merged, slice0[i])
+			i++
+			j++
+		}
+	}
+
+	// Append any remaining elements from slice0 and slice1.
+	for i < len(slice0) {
+		merged = append(merged, slice0[i])
+		i++
+	}
+	for j < len(slice1) {
+		merged = append(merged, slice1[j])
+		j++
+	}
+
+	return merged
+}
---
+++ internal/pkg/offsets/offsets_test.go
@@ -0,0 +1,119 @@
+// Copyright The OpenTelemetry Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package offsets
+
+import (
+	"bytes"
+	"encoding/json"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/go-version"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+var (
+	v12  = version.Must(version.NewVersion("1.2"))
+	v120 = version.Must(version.NewVersion("1.2.0"))
+	v121 = version.Must(version.NewVersion("1.2.1"))
+	v130 = version.Must(version.NewVersion("1.3.0"))
+)
+
+func TestOffsets(t *testing.T) {
+	var o Offsets
+
+	off, ok := o.Get(v120)
+	assert.False(t, ok, "empty offsets found value")
+	assert.Equal(t, uint64(0), off, "empty offset value")
+
+	o.Put(v120, 1)
+	o.Put(v121, 2)
+
+	off, ok = o.Get(v120)
+	assert.True(t, ok, "did not get 1.2.0")
+	assert.Equal(t, uint64(1), off, "invalid value for 1.2.0")
+
+	off, ok = o.Get(v12)
+	assert.True(t, ok, "did not get 1.2")
+	assert.Equal(t, uint64(1), off, "invalid value for 1.2")
+
+	off, ok = o.Get(v121)
+	assert.True(t, ok, "did not get 1.2.1")
+	assert.Equal(t, uint64(2), off, "invalid value for 1.2.1")
+
+	o.Put(v120, 1)
+	off, ok = o.Get(v120)
+	assert.True(t, ok, "did not get 1.2.0 after reset")
+	assert.Equal(t, uint64(1), off, "invalid reset value for 1.2.0")
+
+	o.Put(v120, 2)
+	off, ok = o.Get(v120)
+	assert.True(t, ok, "did not get 1.2.0 after update")
+	assert.Equal(t, uint64(2), off, "invalid update value for 1.2.0")
+}
+
+var index = Index{
+	ID{PkgPath: "net/http", Struct: "Request", Field: "Method"}: &Offsets{
+		values: map[verKey]offsetVersion{
+			newVerKey(v120): {offset: 1, version: v120},
+			newVerKey(v121): {offset: 1, version: v121},
+			newVerKey(v130): {offset: 1, version: v130},
+		},
+	},
+	ID{PkgPath: "net/http", Struct: "Request", Field: "URL"}: &Offsets{
+		values: map[verKey]offsetVersion{
+			newVerKey(v120): {offset: 0, version: v120},
+			newVerKey(v121): {offset: 1, version: v121},
+			newVerKey(v130): {offset: 2, version: v130},
+		},
+	},
+	ID{PkgPath: "net/http", Struct: "Response", Field: "Status"}: &Offsets{
+		values: map[verKey]offsetVersion{
+			newVerKey(v120): {offset: 0, version: v120},
+		},
+	},
+	ID{PkgPath: "google.golang.org/grpc", Struct: "ClientConn", Field: "target"}: &Offsets{
+		values: map[verKey]offsetVersion{
+			newVerKey(v120): {offset: 0, version: v120},
+		},
+	},
+}
+
+func TestIndexMarshalJSON(t *testing.T) {
+	const prefix, indent = "", "  "
+
+	raw, err := os.ReadFile("testdata/offsets.json")
+	require.NoError(t, err)
+
+	// Don't compare whitespace.
+	var buf bytes.Buffer
+	require.NoError(t, json.Indent(&buf, raw, prefix, indent))
+	want := strings.TrimSpace(buf.String())
+
+	got, err := json.MarshalIndent(index, "", "  ")
+	require.NoError(t, err)
+	assert.Equal(t, want, string(got))
+}
+
+func TestIndexUnmarshalJSON(t *testing.T) {
+	f, err := os.Open("testdata/offsets.json")
+	require.NoError(t, err)
+
+	var got Index
+	require.NoError(t, json.NewDecoder(f).Decode(&got))
+	assert.Equal(t, index, got)
+}
