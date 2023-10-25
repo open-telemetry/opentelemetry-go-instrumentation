4ff2846cf00d386650db65d859b0bd260a54462d
Tyler Yahn <codingalias@gmail.com>
2023-10-25 13:06:06 -0700

    Add structfield pkg doc

--- internal/pkg/structfield/structfield.go
+++ internal/pkg/structfield/structfield.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// Package structfield provides types to track struct field offsets.
 package structfield
 
 import (
