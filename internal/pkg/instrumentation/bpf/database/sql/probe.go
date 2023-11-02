1b56f3054eb5a29462b86189a67a0058b5a9299a
Tyler Yahn <codingalias@gmail.com>
2023-11-02 12:47:53 -0700

    Refactor database/sql probe to use base

--- internal/pkg/instrumentation/bpf/database/sql/probe.go
+++ internal/pkg/instrumentation/bpf/database/sql/probe.go
@@ -15,199 +15,127 @@
 package sql
 
 import (
-	"bytes"
-	"encoding/binary"
-	"errors"
 	"os"
 	"strconv"
 
-	"go.opentelemetry.io/auto/internal/pkg/inject"
-	"go.opentelemetry.io/auto/internal/pkg/instrumentation/bpffs"
-	"go.opentelemetry.io/auto/internal/pkg/instrumentation/probe"
-
-	"github.com/cilium/ebpf"
 	"github.com/cilium/ebpf/link"
 	"github.com/cilium/ebpf/perf"
 	"github.com/go-logr/logr"
-	"golang.org/x/sys/unix"
-
 	"go.opentelemetry.io/otel/attribute"
 	semconv "go.opentelemetry.io/otel/semconv/v1.21.0"
 	"go.opentelemetry.io/otel/trace"
+	"golang.org/x/sys/unix"
 
 	"go.opentelemetry.io/auto/internal/pkg/instrumentation/context"
-	"go.opentelemetry.io/auto/internal/pkg/instrumentation/utils"
+	"go.opentelemetry.io/auto/internal/pkg/instrumentation/probe"
 	"go.opentelemetry.io/auto/internal/pkg/process"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go -target amd64,arm64 -cc clang -cflags $CFLAGS bpf ./bpf/probe.bpf.c
 
-const instrumentedPkg = "database/sql"
-
-// Event represents an event in an SQL database
-// request-response.
-type Event struct {
-	context.BaseSpanProperties
-	Query [256]byte
-}
-
-// Probe is the database/sql instrumentation probe.
-type Probe struct {
-	logger       logr.Logger
-	bpfObjects   *bpfObjects
-	uprobes      []link.Link
-	returnProbs  []link.Link
-	eventsReader *perf.Reader
-}
+// name is the instrumentation name.
+const name = "database/sql"
 
 // IncludeDBStatementEnvVar is the environment variable to opt-in for sql query inclusion in the trace.
 const IncludeDBStatementEnvVar = "OTEL_GO_AUTO_INCLUDE_DB_STATEMENT"
 
-// New returns a new [Probe].
-func New(logger logr.Logger) *Probe {
-	return &Probe{logger: logger.WithName("Probe/db")}
-}
-
-// LibraryName returns the database/sql/ package name.
-func (h *Probe) LibraryName() string {
-	return instrumentedPkg
-}
-
-// FuncNames returns the function names from "database/sql" that are instrumented.
-func (h *Probe) FuncNames() []string {
-	return []string{
-		"database/sql.(*DB).queryDC",
-		"database/sql.(*DB).execDC",
-	}
-}
-
-// Load loads all instrumentation offsets.
-func (h *Probe) Load(exec *link.Executable, target *process.TargetDetails) error {
-	spec, err := loadBpf()
-	if err != nil {
-		return err
-	}
-	if target.AllocationDetails == nil {
-		// This Probe requires allocation.
-		return errors.New("no allocation details")
-	}
-	err = inject.Constants(
-		spec,
-		inject.WithRegistersABI(target.IsRegistersABI()),
-		inject.WithAllocationDetails(*target.AllocationDetails),
-		inject.WithKeyValue("should_include_db_statement", shouldIncludeDBStatement()),
-	)
-	if err != nil {
-		return err
-	}
-
-	h.bpfObjects = &bpfObjects{}
-
-	err = utils.LoadEBPFObjects(spec, h.bpfObjects, &ebpf.CollectionOptions{
-		Maps: ebpf.MapOptions{
-			PinPath: bpffs.PathForTargetApplication(target),
+// New returns a new [probe.Probe].
+func New(logger logr.Logger) probe.Probe {
+	return &probe.Base[bpfObjects, event]{
+		Name:            name,
+		Logger:          logger.WithName(name),
+		InstrumentedPkg: "database/sql",
+		Consts: []probe.Const{
+			probe.RegistersABIConst{},
+			probe.AllocationConst{},
+			probe.KeyValConst{
+				Key: "should_include_db_statement",
+				Val: shouldIncludeDBStatement(),
+			},
 		},
-	})
+		Uprobes: map[string]probe.UprobeFunc[bpfObjects]{
+			"database/sql.(*DB).queryDC": uprobeQueryDC,
+			"database/sql.(*DB).execDC":  uprobeExecDC,
+		},
 
-	if err != nil {
-		return err
-	}
-
-	offset, err := target.GetFunctionOffset(h.FuncNames()[0])
-	if err != nil {
-		return err
-	}
-
-	up, err := exec.Uprobe("", h.bpfObjects.UprobeQueryDC, &link.UprobeOptions{
-		Address: offset,
-	})
-	if err != nil {
-		return err
-	}
-
-	h.uprobes = append(h.uprobes, up)
-
-	retOffsets, err := target.GetFunctionReturns(h.FuncNames()[0])
-	if err != nil {
-		return err
-	}
-
-	for _, ret := range retOffsets {
-		retProbe, err := exec.Uprobe("", h.bpfObjects.UprobeQueryDC_Returns, &link.UprobeOptions{
-			Address: ret,
-		})
-		if err != nil {
-			return err
-		}
-		h.returnProbs = append(h.returnProbs, retProbe)
-	}
-
-	offset, err = target.GetFunctionOffset(h.FuncNames()[1])
-	if err != nil {
-		return err
-	}
-
-	up, err = exec.Uprobe("", h.bpfObjects.UprobeExecDC, &link.UprobeOptions{
-		Address: offset,
-	})
-	if err != nil {
-		return err
-	}
-
-	h.uprobes = append(h.uprobes, up)
-
-	retOffsets, err = target.GetFunctionReturns(h.FuncNames()[1])
-	if err != nil {
-		return err
-	}
-
-	for _, ret := range retOffsets {
-		retProbe, err := exec.Uprobe("", h.bpfObjects.UprobeExecDC_Returns, &link.UprobeOptions{
-			Address: ret,
-		})
-		if err != nil {
-			return err
-		}
-		h.returnProbs = append(h.returnProbs, retProbe)
-	}
-
-	rd, err := perf.NewReader(h.bpfObjects.Events, os.Getpagesize())
-	if err != nil {
-		return err
-	}
-	h.eventsReader = rd
-
-	return nil
-}
-
-// Run runs the events processing loop.
-func (h *Probe) Run(eventsChan chan<- *probe.Event) {
-	var event Event
-	for {
-		record, err := h.eventsReader.Read()
-		if err != nil {
-			if errors.Is(err, perf.ErrClosed) {
-				return
-			}
-			h.logger.Error(err, "error reading from perf reader")
-			continue
-		}
-
-		if record.LostSamples != 0 {
-			h.logger.V(0).Info("perf event ring buffer full", "dropped", record.LostSamples)
-			continue
-		}
-
-		if err := binary.Read(bytes.NewBuffer(record.RawSample), binary.LittleEndian, &event); err != nil {
-			h.logger.Error(err, "error parsing perf event")
-			continue
-		}
-
-		eventsChan <- h.convertEvent(&event)
+		ReaderFn: func(obj bpfObjects) (*perf.Reader, error) {
+			return perf.NewReader(obj.Events, os.Getpagesize())
+		},
+		SpecFn:    loadBpf,
+		ProcessFn: convertEvent,
 	}
 }
 
-func (h *Probe) convertEvent(e *Event) *probe.Event {
+func uprobeQueryDC(name string, exec *link.Executable, target *process.TargetDetails, obj *bpfObjects) ([]link.Link, error) {
+	offset, err := target.GetFunctionOffset(name)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := &link.UprobeOptions{Address: offset}
+	l, err := exec.Uprobe("", obj.UprobeQueryDC, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	links := []link.Link{l}
+
+	retOffsets, err := target.GetFunctionReturns(name)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ret := range retOffsets {
+		opts := &link.UprobeOptions{Address: ret}
+		l, err := exec.Uprobe("", obj.UprobeQueryDC_Returns, opts)
+		if err != nil {
+			return nil, err
+		}
+		links = append(links, l)
+	}
+
+	return links, nil
+}
+
+func uprobeExecDC(name string, exec *link.Executable, target *process.TargetDetails, obj *bpfObjects) ([]link.Link, error) {
+	offset, err := target.GetFunctionOffset(name)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := &link.UprobeOptions{Address: offset}
+	l, err := exec.Uprobe("", obj.UprobeExecDC, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	links := []link.Link{l}
+
+	retOffsets, err := target.GetFunctionReturns(name)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, ret := range retOffsets {
+		opts := &link.UprobeOptions{Address: ret}
+		l, err := exec.Uprobe("", obj.UprobeExecDC_Returns, opts)
+		if err != nil {
+			return nil, err
+		}
+		links = append(links, l)
+	}
+
+	return links, nil
+}
+
+// event represents an event in an SQL database
+// request-response.
+type event struct {
+	context.BaseSpanProperties
+	Query [256]byte
+}
+
+func convertEvent(e *event) *probe.Event {
 	query := unix.ByteSliceToString(e.Query[:])
 
 	sc := trace.NewSpanContext(trace.SpanContextConfig{
@@ -230,7 +158,7 @@ func (h *Probe) convertEvent(e *Event) *probe.Event {
 	}
 
 	return &probe.Event{
-		Library:     h.LibraryName(),
+		Library:     name,
 		Name:        "DB",
 		Kind:        trace.SpanKindClient,
 		StartTime:   int64(e.StartTime),
@@ -240,26 +168,6 @@ func (h *Probe) convertEvent(e *Event) *probe.Event {
 			semconv.DBStatementKey.String(query),
 		},
 		ParentSpanContext: pscPtr,
-	}
-}
-
-// Close stops the Probe.
-func (h *Probe) Close() {
-	h.logger.Info("closing database/sql/sql probe")
-	if h.eventsReader != nil {
-		h.eventsReader.Close()
-	}
-
-	for _, r := range h.uprobes {
-		r.Close()
-	}
-
-	for _, r := range h.returnProbs {
-		r.Close()
-	}
-
-	if h.bpfObjects != nil {
-		h.bpfObjects.Close()
 	}
 }
 
