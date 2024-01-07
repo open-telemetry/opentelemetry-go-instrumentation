0d16b5f94ecf53fce06f54e1b4940c9dd3fe83e1
Ron Federman <ron@keyval.dev>
2024-01-07 23:04:22 +0200

    Report errors from closing if exist, change the instrumentation.Close to being synchronous

--- internal/pkg/instrumentation/probe/probe.go
+++ internal/pkg/instrumentation/probe/probe.go
@@ -48,7 +48,7 @@ type Probe interface {
 	Run(eventsChan chan<- *Event)
 
 	// Close stops the Probe.
-	Close()
+	Close() error
 }
 
 // Base is a base implementation of [Probe].
@@ -201,14 +201,15 @@ func (i *Base[BPFObj, BPFEvent]) processRecord(record perf.Record) (*Event, erro
 }
 
 // Close stops the Probe.
-func (i *Base[BPFObj, BPFEvent]) Close() {
+func (i *Base[BPFObj, BPFEvent]) Close() error {
 	var err error
 	for _, c := range i.closers {
 		err = errors.Join(err, c.Close())
 	}
-	if err != nil {
-		i.Logger.Error(err, "failed to cleanup", "Probe", i.Name)
+	if err == nil {
+		i.Logger.Info("Closed", "Probe", i.Name)
 	}
+	return err
 }
 
 // UprobeFunc is a function that will attach a eBPF program to a perf event
