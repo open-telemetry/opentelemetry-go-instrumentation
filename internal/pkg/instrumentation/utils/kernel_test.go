f9aa47a097ade22c983c46ec0415fa8934adf2b1
Nikola Grcevski <6207777+grcevski@users.noreply.github.com>
2024-01-24 10:48:48 -0500

    Detect if the Linux Kernel is in lockdown mode (#620)
    * add function to detect kernel lockdown
    
    * add helper to validate if bpf_probe_write_user can be used
    
    * check for allowed context propagation in http client
    
    * add validation to grpc client
    
    * update change log
    
    * make offsets
    
    ---------
    
    Co-authored-by: Tyler Yahn <MrAlias@users.noreply.github.com>

--- internal/pkg/instrumentation/utils/kernel_test.go
+++ internal/pkg/instrumentation/utils/kernel_test.go
@@ -15,6 +15,8 @@
 package utils
 
 import (
+	"os"
+	"path/filepath"
 	"syscall"
 	"testing"
 
@@ -59,4 +61,57 @@ func TestGetLinuxKernelVersion(t *testing.T) {
 			assert.Equal(t, tt.want, got)
 		})
 	}
+}
+
+func TestLockdownParsing(t *testing.T) {
+	noFile, err := os.CreateTemp("", "not_existent_fake_lockdown")
+	assert.NoError(t, err)
+	notPath, err := filepath.Abs(noFile.Name())
+	assert.NoError(t, err)
+	assert.NoError(t, noFile.Close())
+	assert.NoError(t, os.Remove(noFile.Name()))
+
+	// Setup for testing file that doesn't exist
+	lockdownPath = notPath
+	assert.Equal(t, KernelLockdownNone, KernelLockdownMode())
+
+	tempFile, err := os.CreateTemp("", "fake_lockdown")
+	assert.NoError(t, err)
+	path, err := filepath.Abs(tempFile.Name())
+	assert.NoError(t, err)
+	assert.NoError(t, tempFile.Close())
+
+	defer os.Remove(tempFile.Name())
+	// Setup for testing
+	lockdownPath = path
+
+	setIntegrity(t, path, "none [integrity] confidentiality\n")
+	assert.Equal(t, KernelLockdownIntegrity, KernelLockdownMode())
+
+	setIntegrity(t, path, "[none] integrity confidentiality\n")
+	assert.Equal(t, KernelLockdownNone, KernelLockdownMode())
+
+	setIntegrity(t, path, "none integrity [confidentiality]\n")
+	assert.Equal(t, KernelLockdownConfidentiality, KernelLockdownMode())
+
+	setIntegrity(t, path, "whatever\n")
+	assert.Equal(t, KernelLockdownOther, KernelLockdownMode())
+
+	setIntegrity(t, path, "")
+	assert.Equal(t, KernelLockdownIntegrity, KernelLockdownMode())
+
+	setIntegrity(t, path, "[none] integrity confidentiality\n")
+	setNotReadable(t, path)
+	assert.Equal(t, KernelLockdownIntegrity, KernelLockdownMode())
+}
+
+// Utils.
+func setIntegrity(t *testing.T, path, text string) {
+	err := os.WriteFile(path, []byte(text), 0o644)
+	assert.NoError(t, err)
+}
+
+func setNotReadable(t *testing.T, path string) {
+	err := os.Chmod(path, 0o00)
+	assert.NoError(t, err)
 }
