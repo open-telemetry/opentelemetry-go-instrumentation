036cf02c83bcc4b1e3c186999c5be2bc2290d09e
Tyler Yahn <codingalias@gmail.com>
2024-07-03 13:03:25 -0700

    Upgrade net/http semconv to v1.26.0

--- internal/pkg/instrumentation/bpf/net/http/server/probe_test.go
+++ internal/pkg/instrumentation/bpf/net/http/server/probe_test.go
@@ -22,7 +22,7 @@ import (
 
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/codes"
-	semconv "go.opentelemetry.io/otel/semconv/v1.25.0"
+	semconv "go.opentelemetry.io/otel/semconv/v1.26.0"
 	"go.opentelemetry.io/otel/trace"
 
 	"go.opentelemetry.io/auto/internal/pkg/instrumentation/context"
