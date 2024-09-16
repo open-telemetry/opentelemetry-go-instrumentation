238af1c79e806e4f6324636c1696c6e8b0abf5ec
Tyler Yahn <MrAlias@users.noreply.github.com>
2024-09-16 07:34:11 -0700

    Test Span operations do not panic for nil values (#1099)
    Adds first unit test to the SDK.

---
+++ sdk/trace_test.go
@@ -0,0 +1,49 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+package sdk
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/trace"
+)
+
+var attrs = []attribute.KeyValue{
+	attribute.Bool("bool", true),
+	attribute.Int("int", -1),
+	attribute.Int64("int64", 43),
+	attribute.Float64("float64", 0.3),
+	attribute.String("string", "value"),
+	attribute.BoolSlice("bool slice", []bool{true, false, true}),
+	attribute.IntSlice("int slice", []int{-1, -30, 328}),
+	attribute.Int64Slice("int64 slice", []int64{1030, 0, 0}),
+	attribute.Float64Slice("float64 slice", []float64{1e9}),
+	attribute.StringSlice("string slice", []string{"one", "two"}),
+}
+
+func TestSpanNilUnsampledGuards(t *testing.T) {
+	run := func(fn func(s *span)) func(*testing.T) {
+		return func(t *testing.T) {
+			t.Helper()
+
+			f := func(s *span) func() { return func() { fn(s) } }
+			assert.NotPanics(t, f(nil), "nil span")
+			assert.NotPanics(t, f(new(span)), "unsampled span")
+		}
+	}
+
+	t.Run("End", run(func(s *span) { s.End() }))
+	t.Run("AddEvent", run(func(s *span) { s.AddEvent("event name") }))
+	t.Run("AddLink", run(func(s *span) { s.AddLink(trace.Link{}) }))
+	t.Run("IsRecording", run(func(s *span) { _ = s.IsRecording() }))
+	t.Run("RecordError", run(func(s *span) { s.RecordError(nil) }))
+	t.Run("SpanContext", run(func(s *span) { _ = s.SpanContext() }))
+	t.Run("SetStatus", run(func(s *span) { s.SetStatus(codes.Error, "test") }))
+	t.Run("SetName", run(func(s *span) { s.SetName("span name") }))
+	t.Run("SetAttributes", run(func(s *span) { s.SetAttributes(attrs...) }))
+	t.Run("TracerProvider", run(func(s *span) { _ = s.TracerProvider() }))
+}
