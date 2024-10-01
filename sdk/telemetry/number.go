0d8d5f674ef911472a0b3fbd2ab670fdf7a3ddbe
Tyler Yahn <codingalias@gmail.com>
2024-10-01 14:06:33 -0700

    Add Span type to telemetry pkg

--- sdk/telemetry/number.go
+++ sdk/telemetry/number.go
@@ -36,3 +36,32 @@ func (i *protoInt64) UnmarshalJSON(data []byte) error {
 	}
 	return nil
 }
+
+// protoUint64 represents the protobuf encoding of integers which can be either
+// strings or integers.
+type protoUint64 uint64
+
+// Int64 returns the protoUint64 as a uint64.
+func (i *protoUint64) Uint64() uint64 { return uint64(*i) }
+
+// UnmarshalJSON decodes both strings and integers.
+func (i *protoUint64) UnmarshalJSON(data []byte) error {
+	if data[0] == '"' {
+		var str string
+		if err := json.Unmarshal(data, &str); err != nil {
+			return err
+		}
+		parsedUint, err := strconv.ParseUint(str, 10, 64)
+		if err != nil {
+			return err
+		}
+		*i = protoUint64(parsedUint)
+	} else {
+		var parsedUint uint64
+		if err := json.Unmarshal(data, &parsedUint); err != nil {
+			return err
+		}
+		*i = protoUint64(parsedUint)
+	}
+	return nil
+}
