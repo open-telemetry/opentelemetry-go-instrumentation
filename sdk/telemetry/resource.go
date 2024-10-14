8f7d1dc85b20178e1589f61438e916bc68ada462
Tyler Yahn <codingalias@gmail.com>
2024-10-14 09:54:46 -0700

    Custom UnmarshalJSON for Resource
    Handle droppedAttributesCount and dropped_attributes_count.

--- sdk/telemetry/resource.go
+++ sdk/telemetry/resource.go
@@ -3,6 +3,14 @@
 
 package telemetry
 
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+)
+
 // Resource information.
 type Resource struct {
 	// Attrs are the set of attributes that describe the resource. Attribute
@@ -12,4 +20,47 @@ type Resource struct {
 	// DroppedAttrs is the number of dropped attributes. If the value
 	// is 0, then no attributes were dropped.
 	DroppedAttrs uint32 `json:"droppedAttributesCount,omitempty"`
+}
+
+// UnmarshalJSON decodes the OTLP formatted JSON contained in data into r.
+func (r *Resource) UnmarshalJSON(data []byte) error {
+	decoder := json.NewDecoder(bytes.NewReader(data))
+
+	t, err := decoder.Token()
+	if err != nil {
+		return err
+	}
+	if t != json.Delim('{') {
+		return errors.New("invalid Resource type")
+	}
+
+	for decoder.More() {
+		keyIface, err := decoder.Token()
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				// Empty.
+				return nil
+			}
+			return err
+		}
+
+		key, ok := keyIface.(string)
+		if !ok {
+			return fmt.Errorf("invalid Resource field: %#v", keyIface)
+		}
+
+		switch key {
+		case "attributes":
+			err = decoder.Decode(&r.Attrs)
+		case "droppedAttributesCount", "dropped_attributes_count":
+			err = decoder.Decode(&r.DroppedAttrs)
+		default:
+			// Skip unknown.
+		}
+
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
