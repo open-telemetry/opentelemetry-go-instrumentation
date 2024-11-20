7ca91a01ca493fc17f9a62405f4214f0f71216c8
Tyler Yahn <MrAlias@users.noreply.github.com>
2024-11-20 07:48:16 -0800

    Move `spanKind` to `tracer.go` (#1304)
    * Move spanKind to only use
    
    The spanKind func is used by `tracer.Start` and nowhere else. Move its
    definition closer to its use and similarly adjust the tests.
    
    * Fix lint

--- sdk/trace.go
+++ sdk/trace.go
@@ -21,22 +21,6 @@ import (
 	"go.opentelemetry.io/auto/sdk/internal/telemetry"
 )
 
-func spanKind(kind trace.SpanKind) telemetry.SpanKind {
-	switch kind {
-	case trace.SpanKindInternal:
-		return telemetry.SpanKindInternal
-	case trace.SpanKindServer:
-		return telemetry.SpanKindServer
-	case trace.SpanKindClient:
-		return telemetry.SpanKindClient
-	case trace.SpanKindProducer:
-		return telemetry.SpanKindProducer
-	case trace.SpanKindConsumer:
-		return telemetry.SpanKindConsumer
-	}
-	return telemetry.SpanKind(0) // undefined.
-}
-
 type span struct {
 	noop.Span
 
