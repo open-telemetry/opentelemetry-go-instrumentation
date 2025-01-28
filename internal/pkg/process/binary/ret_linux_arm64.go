95c6834b1dd121e78b269aed58a52ceee9ed9e31
Ron Federman <ron@keyval.dev>
2025-01-28 16:01:41 +0200

    fix lint

--- internal/pkg/process/binary/ret_linux_arm64.go
+++ internal/pkg/process/binary/ret_linux_arm64.go
@@ -7,6 +7,8 @@
 package binary
 
 import (
+	"fmt"
+
 	"golang.org/x/arch/arm64/arm64asm"
 )
 
@@ -16,12 +18,18 @@ const (
 )
 
 func findRetInstructions(data []byte) ([]uint64, error) {
+	nInt := len(data)
+	if nInt < 0 {
+		return nil, fmt.Errorf("invalid data length: %d", nInt)
+	}
+	n := uint64(nInt)
+
 	var returnOffsets []uint64
-	index := 0
-	for index < len(data) {
+	var index uint64
+	for index < n {
 		instruction, err := arm64asm.Decode(data[index:])
 		if err == nil && instruction.Op == arm64asm.RET {
-			returnOffsets = append(returnOffsets, uint64(index))
+			returnOffsets = append(returnOffsets, index)
 		}
 
 		index += armInstructionSize
