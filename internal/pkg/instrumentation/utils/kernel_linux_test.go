e1a93173f2deb58438338328d09eb480b5065e7b
Tyler Yahn <codingalias@gmail.com>
2025-02-06 13:08:26 -0800

    Replace use of github.com/hashicorp/go-version
    Replace with github.com/Masterminds/semver/v3
    
    Resolve #1753

--- internal/pkg/instrumentation/utils/kernel_linux_test.go
+++ internal/pkg/instrumentation/utils/kernel_linux_test.go
@@ -9,28 +9,29 @@ import (
 	"syscall"
 	"testing"
 
-	"github.com/hashicorp/go-version"
+	"github.com/Masterminds/semver/v3"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestGetLinuxKernelVersion(t *testing.T) {
 	tests := map[string]struct {
 		unameFn func(buf *syscall.Utsname) error
-		want    *version.Version
+		want    *semver.Version
 	}{
 		"ubuntu-23.10": {
 			unameFn: func(buf *syscall.Utsname) error {
 				buf.Release = [65]int8{54, 46, 53, 46, 48, 45, 57, 45, 103, 101, 110, 101, 114, 105, 99, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 				return nil
 			},
-			want: version.Must(version.NewVersion("6.5")),
+			want: semver.New(6, 5, 0, "", ""),
 		},
 		"debian-12": {
 			unameFn: func(buf *syscall.Utsname) error {
 				buf.Release = [65]int8{54, 46, 49, 46, 48, 45, 49, 50, 45, 99, 108, 111, 117, 100, 45, 97, 109, 100, 54, 52, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 				return nil
 			},
-			want: version.Must(version.NewVersion("6.1")),
+			want: semver.New(6, 1, 0, "", ""),
 		},
 	}
 	for name, tt := range tests {
@@ -41,11 +42,8 @@ func TestGetLinuxKernelVersion(t *testing.T) {
 			t.Cleanup(func() {
 				unameFn = oldUnameFn
 			})
-			got, err := GetLinuxKernelVersion()
-			if err != nil {
-				t.Errorf("GetLinuxKernelVersion() error = %v", err)
-				return
-			}
+			got := GetLinuxKernelVersion()
+			require.NotNil(t, got)
 
 			assert.Equal(t, tt.want, got)
 		})
