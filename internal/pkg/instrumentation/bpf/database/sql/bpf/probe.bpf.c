394d9a0e4fe5216b1a90eb19f1f9b27091bfa98f
Tyler Yahn <codingalias@gmail.com>
2025-02-07 11:20:06 -0800

    Fix database instrumentation for Go < 1.17
    Use the correct context argument position in the UPROBE_RETURN macro.

--- internal/pkg/instrumentation/bpf/database/sql/bpf/probe.bpf.c
+++ internal/pkg/instrumentation/bpf/database/sql/bpf/probe.bpf.c
@@ -69,7 +69,7 @@ int uprobe_queryDC(struct pt_regs *ctx) {
 
 // This instrumentation attaches uprobe to the following function:
 // func (db *DB) queryDC(ctx, txctx context.Context, dc *driverConn, releaseConn func(error), query string, args []any)
-UPROBE_RETURN(queryDC, struct sql_request_t, sql_events, events, 3, 0, true)
+UPROBE_RETURN(queryDC, struct sql_request_t, sql_events, events, 2, 0, true)
 
 // This instrumentation attaches uprobe to the following function:
 // func (db *DB) execDC(ctx context.Context, dc *driverConn, release func(error), query string, args []any)
