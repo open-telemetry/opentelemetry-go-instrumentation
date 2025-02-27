1613b0c0029c096d1f939f1b15b0f9dc907ae590
Tyler Yahn <codingalias@gmail.com>
2025-02-27 08:16:15 -0800

    Return an error from Handle

--- internal/pkg/instrumentation/manager_test.go
+++ internal/pkg/instrumentation/manager_test.go
@@ -220,7 +220,7 @@ type noopHandler struct{}
 var _ export.Handler = noopHandler{}
 
 // Handle drops the passed telemetry.
-func (noopHandler) Handle(*export.Telemetry) {}
+func (noopHandler) Handle(*export.Telemetry) error { return nil }
 
 func TestRunStoppingByContext(t *testing.T) {
 	probeStop := make(chan struct{})
@@ -521,7 +521,7 @@ func (p *hangingProbe) Run(h export.Handler) {
 	<-p.closeReturned
 	// Write after Close has returned.
 	t := new(export.Telemetry)
-	h.Handle(t)
+	_ = h.Handle(t)
 }
 
 func (p *hangingProbe) Close() error {
