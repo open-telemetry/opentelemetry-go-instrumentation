008e3cf06f6553931eae481fbcc3d26fba0d4448
Tyler Yahn <codingalias@gmail.com>
2025-03-05 08:34:18 -0800

    Add NewInfo to process pkg
    Replace the Analyze method of the Analyzer with the new function.

--- internal/pkg/process/analyze.go
+++ internal/pkg/process/analyze.go
@@ -7,6 +7,7 @@ import (
 	"debug/elf"
 	"errors"
 	"fmt"
+	"runtime/debug"
 
 	"github.com/Masterminds/semver/v3"
 
@@ -20,6 +21,79 @@ type Info struct {
 	GoVersion  *semver.Version
 	Modules    map[string]*semver.Version
 	Allocation *Allocation
+}
+
+// NewInfo returns a new Info with information about the process identified by
+// id. The functions of the returned Info are filtered by relevantFuncs.
+//
+// A partial Info and error may be returned for dependencies that cannot be
+// parsed.
+func NewInfo(id ID, relevantFuncs map[string]interface{}) (*Info, error) {
+	elfF, err := elf.Open(id.ExePath())
+	if err != nil {
+		return nil, err
+	}
+	defer elfF.Close()
+
+	bi, err := id.BuildInfo()
+	if err != nil {
+		return nil, err
+	}
+
+	goVersion, err := semver.NewVersion(bi.GoVersion)
+	if err != nil {
+		return nil, err
+	}
+
+	result := &Info{
+		ID:        id,
+		GoVersion: goVersion,
+	}
+
+	result.Functions, err = findFunctions(elfF, relevantFuncs)
+	if err != nil {
+		return nil, err
+	}
+
+	result.Modules, err = findModules(goVersion, bi.Deps)
+	return result, err
+}
+
+func findModules(goVer *semver.Version, deps []*debug.Module) (map[string]*semver.Version, error) {
+	var err error
+	out := make(map[string]*semver.Version, len(deps)+1)
+	for _, dep := range deps {
+		depVersion, e := semver.NewVersion(dep.Version)
+		if e != nil {
+			err = errors.Join(
+				err,
+				fmt.Errorf("invalid dependency version %s (%s): %w", dep.Path, dep.Version, e),
+			)
+			continue
+		}
+		out[dep.Path] = depVersion
+	}
+	out["std"] = goVer
+	return out, err
+}
+
+func findFunctions(elfF *elf.File, relevantFuncs map[string]interface{}) ([]*binary.Func, error) {
+	found, err := binary.FindFunctionsUnStripped(elfF, relevantFuncs)
+	if err != nil {
+		if !errors.Is(err, elf.ErrNoSymbols) {
+			return nil, err
+		}
+		found, err = binary.FindFunctionsStripped(elfF, relevantFuncs)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if len(found) == 0 {
+		return nil, errors.New("no functions found")
+	}
+
+	return found, nil
 }
 
 // GetFunctionOffset returns the offset for of the function with name.
@@ -43,64 +117,4 @@ func (i *Info) GetFunctionReturns(name string) ([]uint64, error) {
 	}
 
 	return nil, fmt.Errorf("could not find returns for function %s", name)
-}
-
-// Analyze returns the target details for an actively running process.
-func (a *Analyzer) Analyze(relevantFuncs map[string]interface{}) (*Info, error) {
-	result := &Info{ID: a.id}
-
-	elfF, err := elf.Open(a.id.ExePath())
-	if err != nil {
-		return nil, err
-	}
-	defer elfF.Close()
-
-	bi, err := a.id.BuildInfo()
-	if err != nil {
-		return nil, err
-	}
-
-	goVersion, err := semver.NewVersion(bi.GoVersion)
-	if err != nil {
-		return nil, err
-	}
-	result.GoVersion = goVersion
-	result.Modules = make(map[string]*semver.Version, len(bi.Deps)+1)
-	for _, dep := range bi.Deps {
-		depVersion, err := semver.NewVersion(dep.Version)
-		if err != nil {
-			a.logger.Error("parsing dependency version", "error", err, "dependency", dep)
-			continue
-		}
-		result.Modules[dep.Path] = depVersion
-	}
-	result.Modules["std"] = goVersion
-
-	funcs, err := a.findFunctions(elfF, relevantFuncs)
-	if err != nil {
-		return nil, err
-	}
-	for _, fn := range funcs {
-		a.logger.Debug("found function", "function_name", fn)
-	}
-
-	result.Functions = funcs
-	if len(result.Functions) == 0 {
-		return nil, errors.New("could not find function offsets for instrumenter")
-	}
-
-	return result, nil
-}
-
-func (a *Analyzer) findFunctions(elfF *elf.File, relevantFuncs map[string]interface{}) ([]*binary.Func, error) {
-	result, err := binary.FindFunctionsUnStripped(elfF, relevantFuncs)
-	if err != nil {
-		if errors.Is(err, elf.ErrNoSymbols) {
-			a.logger.Debug("No symbols found in binary, trying to find functions using .gosymtab")
-			return binary.FindFunctionsStripped(elfF, relevantFuncs)
-		}
-		return nil, err
-	}
-
-	return result, nil
 }
