c7371e54fa0998490f7f333a7a266094f2b1d92e
Tyler Yahn <MrAlias@users.noreply.github.com>
2025-03-14 09:21:37 -0700

    Stop adding `process.runtime.description` to resource (#1986)
    * Stop adding process.runtime.description to resource
    
    Fix #1983
    
    * Update test expectations
    
    * Add a changelog entry
    
    * Remove runtime desc attr fix for e2e test

--- internal/test/test_helpers/utilities.sh
+++ internal/test/test_helpers/utilities.sh
@@ -161,12 +161,6 @@ redact_json() {
 				end
 			| .resourceSpans[].scopeSpans|=sort_by(.scope.name)
 			| .resourceSpans[].scopeSpans[].spans|=sort_by(.kind)
-			| .resourceSpans[].resource.attributes[]? |= if 
-					(.key == "process.runtime.description") then
-					.value.stringValue |= sub("amd64|arm64"; "VALID_ARCH")
-				else
-					.
-				end
 			' > ${BATS_TEST_DIRNAME}/traces.json
 }
 
