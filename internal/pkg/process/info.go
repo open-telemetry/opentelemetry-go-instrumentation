1b1f0cc42f4cbe362433505cb289795c7c2e23dc
Tyler Yahn <codingalias@gmail.com>
2025-03-14 14:08:12 -0700

    Refactor TestLoadProbes

--- internal/pkg/process/info.go
+++ internal/pkg/process/info.go
@@ -52,7 +52,7 @@ func NewInfo(id ID, relevantFuncs map[string]interface{}) (*Info, error) {
 
 	result.Functions, err = findFunctions(elfF, relevantFuncs)
 	if err != nil {
-		return nil, err
+		return result, err
 	}
 
 	result.Modules, err = findModules(goVersion, bi.Deps)
