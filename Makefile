4e59ce136bf5b090d3665adbc69716490757f513
Tyler Yahn <codingalias@gmail.com>
2025-03-17 10:00:04 -0700

    Add nonrecording E2E test

--- Makefile
+++ Makefile
@@ -185,7 +185,7 @@ license-header-check:
 	           exit 1; \
 	   fi
 
-.PHONY: fixture-nethttp fixture-gin fixture-databasesql fixture-nethttp-custom fixture-otelglobal fixture-autosdk fixture-kafka-go
+.PHONY: fixture-nethttp fixture-gin fixture-databasesql fixture-nethttp-custom fixture-otelglobal fixture-autosdk fixture-nonrecording fixture-kafka-go
 fixture-nethttp-custom: fixtures/nethttp_custom
 fixture-nethttp: fixtures/nethttp
 fixture-gin: fixtures/gin
@@ -193,6 +193,7 @@ fixture-databasesql: fixtures/databasesql
 fixture-grpc: fixtures/grpc
 fixture-otelglobal: fixtures/otelglobal
 fixture-autosdk: fixtures/autosdk
+fixture-nonrecording: fixtures/nonrecording
 fixture-kafka-go: fixtures/kafka-go
 fixtures/%: LIBRARY=$*
 fixtures/%: docker-build
---
+++ internal/test/e2e/nonrecording/build.sh
@@ -0,0 +1,114 @@
+#!/usr/bin/env bash
+
+# Copyright The OpenTelemetry Authors
+# SPDX-License-Identifier: Apache-2.0
+
+usage() {
+    local progname
+    progname="$( basename "$0" )"
+
+    cat <<-EOF
+    Usage: $progname [OPTIONS]
+
+    Builds the SDK end-to-end testing docker image.
+
+    OPTIONS:
+        -t --tag            Docker tag to use ["sample-app"]
+        -h --help           Show this help message
+EOF
+}
+
+parse_opts() {
+    # Make sure getopts starts at the beginning
+    OPTIND=1
+
+    local deliminator option
+    local arg=
+
+    # Translate --gnu-long-options to -g (short options)
+    for arg
+    do
+        deliminator=""
+        case "$arg" in
+            --tag)
+                args="${args}-d "
+                ;;
+            --help)
+                args="${args}-h "
+                ;;
+            *)
+                [[ "${arg:0:1}" == "-" ]] || deliminator='"'
+                args="${args}${deliminator}${arg}${deliminator} "
+                ;;
+        esac
+    done
+
+    # Reset the positional parameters to start parsing short options
+    eval set -- "$args"
+
+    while getopts ":t:h" option
+    do
+        case "$option" in
+            t)
+                readonly TAG="$OPTARG"
+                ;;
+            h)
+                usage
+                exit 0
+                ;;
+            *)
+                echo "Invalid option: -${option}" >&2
+                usage
+                exit 1
+                ;;
+        esac
+    done
+
+    # Default values
+    [ -z "${TAG}" ] \
+        && readonly TAG="sample-app"
+
+    return 0
+}
+
+build() {
+    local root_dir="$1"
+    local local_dir="$2"
+    local dockerfile="${local_dir}/Dockerfile"
+    local tag_arg
+
+    if [ ! -f "$dockerfile" ]; then
+        echo "Dockerfile does not exist: $dockerfile"
+        return 1
+    fi
+
+    if [ ! -d "$root_dir" ]; then
+        echo "Project root directory does not exist: $root_dir"
+        return 1
+    fi
+
+    if [ -n "$TAG" ]; then
+        tag_arg=("-t" "$TAG")
+    fi
+
+    (cd "$root_dir" && docker build "${tag_arg[@]}" -f "$dockerfile" .)
+    return 0
+}
+
+main() {
+    local root_dir script_dir
+
+    # Check dependencies
+    hash git 2>/dev/null\
+        || { echo >&2 "Required git client not found"; exit 1; }
+    hash docker 2>/dev/null\
+        || { echo >&2 "Required docker client not found"; exit 1; }
+
+    parse_opts "$@"
+
+    root_dir=$( git rev-parse --show-toplevel )
+    script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+    build "$root_dir" "$script_dir"
+}
+
+main "$@"
