a6b740722f0694bc8dd43b9d9bc127fb006faf84
Tyler Yahn <MrAlias@users.noreply.github.com>
2025-04-27 08:05:01 -0700

    Replace BATS in kafka-go end-to-end test (#2217)
    Part of a resolution for #2137.
    
    This is a one-to-one replacement of the BATS testing as much as possible
    with the one exception being that all producer spans are compared to the
    consumer spans' parent span ID. This way when there is an ordering
    difference and the parent is not the first span we do not introduce a
    flaky test (like we have seen).

--- internal/test/e2e/kafka-go/integration_test.go
+++ internal/test/e2e/kafka-go/integration_test.go
@@ -5,11 +5,188 @@
 package autosdk
 
 import (
+	"encoding/hex"
+	"regexp"
+	"strconv"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.opentelemetry.io/collector/pdata/ptrace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.30.0"
+	"go.uber.org/goleak"
 
 	"go.opentelemetry.io/auto/internal/test/e2e"
 )
 
+// scopeName defines the instrumentation scope name used in the trace.
+const scopeName = "go.opentelemetry.io/auto/github.com/segmentio/kafka-go"
+
 func TestIntegration(t *testing.T) {
-	e2e.TestIntegration(t, "./cmd", ".")
+	if testing.Short() {
+		t.Skip("skipping long-running integration test in short mode.")
+	}
+
+	defer goleak.VerifyNone(t)
+
+	traces := e2e.RunInstrumentation(t, "./cmd")
+	scopes := e2e.ScopeSpansByName(traces, scopeName)
+	require.NotEmpty(t, scopes)
+
+	t.Run("ResourceAttribute/ServiceName", func(t *testing.T) {
+		val, err := e2e.ResourceAttribute(traces, "service.name")
+		require.NoError(t, err)
+		assert.Equal(t, "sample-app", val.AsString())
+	})
+
+	spans := ptrace.NewSpanSlice()
+	for i, scope := range scopes {
+		t.Run("Scope/"+strconv.Itoa(i), func(t *testing.T) {
+			assert.Equal(t, scopeName, scope.Scope().Name(), "scope name")
+			assert.Equal(t, semconv.SchemaURL, scope.SchemaUrl(), "schema URL")
+
+			scope.Spans().MoveAndAppendTo(spans)
+		})
+	}
+
+	// All trace ID should be the same.
+	tIDBytes := [16]byte(spans.At(0).TraceID())
+	tID := hex.EncodeToString(tIDBytes[:])
+
+	var producerSpanIDs []string
+	var cCnt int
+	for j := range spans.Len() {
+		b := [16]byte(spans.At(j).TraceID())
+		assert.Equalf(t, tID, hex.EncodeToString(b[:]), "Span %d: trace ID", j)
+
+		span := spans.At(j)
+		switch span.Kind() {
+		case ptrace.SpanKindProducer:
+			n := len(producerSpanIDs)
+
+			b := [8]byte(span.SpanID())
+			sID := hex.EncodeToString(b[:])
+			producerSpanIDs = append(producerSpanIDs, sID)
+
+			t.Run("ProducerSpan/"+strconv.Itoa(n), pSpan(span))
+		case ptrace.SpanKindConsumer:
+			t.Run("ConsumerSpan/"+strconv.Itoa(cCnt), cSpan(span))
+			cCnt++
+		default:
+			t.Errorf("unexpected span kind: %v", span.Kind())
+		}
+	}
+
+	if cCnt == 0 {
+		t.Error("no consumer span found")
+		return
+	}
+
+	if len(producerSpanIDs) == 0 {
+		t.Error("no producer span found")
+		return
+	}
+
+	cCnt = 0
+	for j := range spans.Len() {
+		span := spans.At(j)
+		if span.Kind() == ptrace.SpanKindConsumer {
+			t.Run(
+				"ConsumerSpan/"+strconv.Itoa(cCnt)+"/ParentSpanID",
+				func(t *testing.T) {
+					b := [8]byte(span.ParentSpanID())
+					sID := hex.EncodeToString(b[:])
+					assert.Contains(t, producerSpanIDs, sID)
+				},
+			)
+			cCnt++
+		}
+	}
+}
+
+var (
+	producerNameRE = regexp.MustCompile(`^topic\d publish$`)
+	topicRe        = regexp.MustCompile(`^topic\d$`)
+	keyRe          = regexp.MustCompile(`^key\d$`)
+)
+
+func pSpan(span ptrace.Span) func(t *testing.T) {
+	return func(t *testing.T) {
+		e2e.AssertTraceID(t, span.TraceID(), "trace ID")
+		e2e.AssertSpanID(t, span.SpanID(), "span ID")
+
+		assert.Equal(t, ptrace.SpanKindProducer, span.Kind(), "span kind")
+
+		assert.Regexp(t, producerNameRE, span.Name(), "span name")
+
+		attrs := e2e.AttributesMap(span.Attributes())
+		assert.Equal(
+			t,
+			"kafka",
+			attrs[string(semconv.MessagingSystemKey)].Str(),
+			"messaging.system",
+		)
+		assert.Regexp(
+			t,
+			topicRe,
+			attrs[string(semconv.MessagingDestinationNameKey)].Str(),
+			"messaging.destination.name",
+		)
+		assert.Regexp(
+			t,
+			keyRe,
+			attrs[string(semconv.MessagingKafkaMessageKeyKey)].Str(),
+			"messaging.kafka.message.key",
+		)
+		assert.Equal(
+			t,
+			int64(2),
+			attrs[string(semconv.MessagingBatchMessageCountKey)].Int(),
+			"messaging.batch.message.count",
+		)
+	}
+}
+
+func cSpan(span ptrace.Span) func(t *testing.T) {
+	return func(t *testing.T) {
+		e2e.AssertTraceID(t, span.TraceID(), "trace ID")
+		e2e.AssertSpanID(t, span.SpanID(), "span ID")
+		e2e.AssertSpanID(t, span.ParentSpanID(), "parent span ID")
+
+		assert.Equal(t, ptrace.SpanKindConsumer, span.Kind(), "span kind")
+
+		assert.Equal(t, "topic1 receive", span.Name(), "span name")
+
+		attrs := e2e.AttributesMap(span.Attributes())
+		assert.Equal(
+			t,
+			"kafka",
+			attrs[string(semconv.MessagingSystemKey)].Str(),
+			"messaging.system",
+		)
+		assert.Equal(
+			t,
+			"topic1",
+			attrs[string(semconv.MessagingDestinationNameKey)].Str(),
+			"messaging.destination.name",
+		)
+		assert.Equal(
+			t,
+			"key1",
+			attrs[string(semconv.MessagingKafkaMessageKeyKey)].Str(),
+			"messaging.kafka.message.key",
+		)
+		assert.Equal(
+			t,
+			"0",
+			attrs[string(semconv.MessagingDestinationPartitionIDKey)].Str(),
+			"messaging.destination.partition.id",
+		)
+		assert.Equal(
+			t,
+			"some group id",
+			attrs[string(semconv.MessagingConsumerGroupNameKey)].Str(),
+			"messaging.consumer.group.name",
+		)
+	}
 }
