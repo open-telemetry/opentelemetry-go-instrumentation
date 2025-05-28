729387858eee2187097f9e1446450f8155d47ef6
Ron Federman <ron@odigos.io>
2025-05-28 20:28:47 +0300

    add distro version and name by default to otelsdk handler

---
+++ internal/pkg/instrumentation/version.go
@@ -0,0 +1,13 @@
+// Copyright The OpenTelemetry Authors
+// SPDX-License-Identifier: Apache-2.0
+
+// Package instrumentation provides functionality to manage instrumentation
+// using eBPF for Go programs.
+package instrumentation
+
+const (
+	// DistributionName is used for `telemetry.distro.name` resource attribute.
+	DistributionName = "opentelemetry-go-instrumentation"
+	// DistributionVersion is the current release version of OpenTelemetry Go auto-instrumentation in use.
+	DistributionVersion = "v0.22.0"
+)
