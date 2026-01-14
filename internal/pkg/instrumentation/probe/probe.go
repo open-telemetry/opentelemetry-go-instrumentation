02a90d8cbeaacdd6e30b96f9a6545660ddbc7f8f
Tyler Yahn <codingalias@gmail.com>
2026-01-14 10:01:46 -0800

    Fix lint

--- internal/pkg/instrumentation/probe/probe.go
+++ internal/pkg/instrumentation/probe/probe.go
@@ -304,7 +304,11 @@ func (i *Base[BPFObj, BPFEvent]) read() (*BPFEvent, error) {
 	} else {
 		var zero BPFEvent
 		if len(record.RawSample) < int(unsafe.Sizeof(zero)) {
-			return nil, fmt.Errorf("record size %d too small for event size %d", len(record.RawSample), unsafe.Sizeof(zero))
+			return nil, fmt.Errorf(
+				"record size %d too small for event size %d",
+				len(record.RawSample),
+				unsafe.Sizeof(zero),
+			)
 		}
 		event = (*BPFEvent)(unsafe.Pointer(&record.RawSample[0]))
 	}
